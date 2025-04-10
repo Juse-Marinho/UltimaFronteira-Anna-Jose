28ddfe454c55f89f5c394d1de6aa689d3b00cefb
Marinho <josepedrohenriqueblmarinho@gmail.com>
2025-04-10 17:15:18 -0300

    Movimentação

---
+++ src/Tile.java
@@ -0,0 +1,13 @@
+public class Tile {
+
+    private int xPos;
+    private int yPos;
+    private Ambiente ambiente;
+
+    public Tile(int xPos, int yPos, Ambiente ambiente){
+        xPos = this.xPos;
+        yPos = this.yPos;
+        ambiente = this.ambiente;
+    }
+
+}
